2479b41b32e48f5d18b9cd00e255ebfbfc7a5543
gotonis <guertin.person@gmail.com>
2014-11-13 15:30:15 -0500

    Changed FCFS to queue and scheduler to general containers

--- src/FCFS_Scheduler.java
+++ src/FCFS_Scheduler.java
@@ -3,25 +3,32 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class FCFS_Scheduler extends Scheduler
 {
 
+	private ConcurrentLinkedQueue<Process> q;
 	public FCFS_Scheduler( Process[] arr ) 
 	{
 		super(new ConcurrentLinkedQueue<Process>(), arr );
+		q = (ConcurrentLinkedQueue<Process>)processes; //MG: To avoid having to cast 500 times
 	}
-	
-	protected Process chooseNext() //MP: Function that determines which process should execute on the CPU.
-	{
-		for( Process p: processes )
-		{
-			if( p.getSTATE() == PROCESS_STATE.ACTIVE_CPU )
-				return p;
+
+	protected Process chooseNext() {
+
+		if(q.peek().getSTATE() == PROCESS_STATE.ACTIVE_CPU || q.peek().getSTATE() == PROCESS_STATE.WAITING_CPU){
+			return q.peek();
 		}
-		
-		for( Process p: processes )
-		{
-			if( p.getSTATE() == PROCESS_STATE.WAITING_CPU )
-				return p;
+		else{
+			if(q.peek().getSTATE() == PROCESS_STATE.FINISHED){
+				q.remove();
+			}
+			else {
+				for(int i = 0; i < q.size(); i++){
+					Process temp = q.remove();
+					q.add(temp);
+					if(q.peek().getSTATE() == PROCESS_STATE.WAITING_CPU){
+						return q.peek();
+					}
+				}
+			}
 		}
-				
 		return null;
 	}
 }
