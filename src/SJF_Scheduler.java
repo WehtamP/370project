bd2b6826c3dfda3d3711ca339766a2a0ad6dc6f3
gotonis <guertin.person@gmail.com>
2014-11-16 21:25:03 -0500

    SJF is most likely good.

--- src/SJF_Scheduler.java
+++ src/SJF_Scheduler.java
@@ -14,21 +14,30 @@ public class SJF_Scheduler extends Scheduler {
 		
 		//MG: Ensures nothing 0/negative goes through
 		if(p2.getCPU_BURST() <= 0){
+			System.out.println("2 was done");
 			p2.setSTATE(PROCESS_STATE.FINISHED);
 			return false;
 		}
 		
 		//MG: If nothing has been selected yet, default to the applicable competitor
-				if(p1 == null)
+				if(p1 == null){
+					System.out.println("1 was null");
 					return true;
+				}
 		
+				System.out.println("Comparing " + p1.getP_ID() + " & " + p2.getP_ID());
+				
 		//MG: If the current process is active, don't overwrite it.
-		if(p1.getSTATE() == PROCESS_STATE.ACTIVE_CPU)
+		if(p1.getSTATE() == PROCESS_STATE.ACTIVE_CPU){
+			System.out.println("1 was active");
 			return false;
+		}
 		
 		//MG: If p2 is active, choose it.
-		if(p2.getSTATE() == PROCESS_STATE.ACTIVE_CPU)
+		if(p2.getSTATE() == PROCESS_STATE.ACTIVE_CPU){
+			System.out.println("2 was active");
 			return true;
+		}
 		
 		//MG: Blocks anything with a CPU Burst of 0 or lower
 		if(p2.getSTATE() == PROCESS_STATE.FINISHED){
@@ -38,14 +47,17 @@ public class SJF_Scheduler extends Scheduler {
 					
 		//MG: Choose the one with a lower positive CPU_BURST
 		if(p1.getCPU_BURST() < p2.getCPU_BURST() && p1.getCPU_BURST() > 0){
+			System.out.println("1 was lower");
 			return false;
 		}
-		else if(p1.getCPU_BURST() > p2.getCPU_BURST() && p2.getCPU_BURST() < 0){
+		else if(p1.getCPU_BURST() > p2.getCPU_BURST() && p2.getCPU_BURST() > 0){
+			System.out.println("2 was lower");
 			return true;
 		}
 		else {
 			
 			//MG: Bakery choice
+			System.out.println("bakery");
 			return (p1.getP_ID() > p2.getP_ID());
 		}
 	}
