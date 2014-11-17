cbbf59ac7d611c31c4560aa3f83921c660582535
gotonis <guertin.person@gmail.com>
2014-11-16 22:58:47 -0500

    Added rts.updateViolations

--- src/RTScheduler.java
+++ src/RTScheduler.java
@@ -3,11 +3,24 @@ import java.util.Collection;
 
 public abstract class RTScheduler extends Scheduler {
 
+	protected int violations;
+
 	//MG: Subclass of scheduler for any commonalities between PRM and EDF
 	RTScheduler(Collection<Process> eColl, Process procs[]){
 		super(eColl, procs);
+		violations = 0;
 	}
 
 	
-	
+	//MG: Violation checker. NOTE: MAKE SURE TO RUN THIS AFTER ACTING BUT BEFORE REMOVING FINISHED PROCESSES
+	protected void updateViolations(){
+		for(Process p:processes){
+			if(p.getPERIOD() == -1) //Hits -1 exactly one time per violation
+				violations++;
+			
+		}
+		if(processor.getPERIOD() == -1)
+			violations++;
+	}
+
 }
